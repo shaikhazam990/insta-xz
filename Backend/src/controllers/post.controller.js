8944eaeda261446cb5ad6aa1adede4f2e62075e5
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-21 02:08:23 +0530

    create post like details

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,9 +1,8 @@
 const postModel = require("../model/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
-const jwt = require("jsonwebtoken")
 
-const imagekit = new ImageKit({
+const client = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 async function createPostController(req, res) {
@@ -26,7 +25,74 @@ const file =await client.files.upload({
     })
 }
 
+async function getPostController(req, res) {
+    const userId = req.user.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200)
+        .json({
+            message: "Posts fetched successfully.",
+            posts
+        })
+
+}
+
+async function getPostDetailsController(req, res) {
+    const userId = req.user.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    return res.status(200).json({
+        message: "Post fetched  successfully.",
+        post
+    })
+}
+
+async function likePostController(req, res) {
+    const username = req.user.username
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const like = await likeModel.create({
+        post: postId,
+        user: username
+    })
+
+    res.status(200).json({
+        message: "Post liked successfully.",
+        like
+    })
+}
+
 
 module.exports ={
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController,
+    likePostController
 }
