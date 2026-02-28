2b22ce8af70c45ca4175930b72a80fae7d3f1975
shaikhazam990 <shaikhazam0990@gmail.com>
2026-03-01 04:07:49 +0530

    render

--- Frontend/src/features/posts/pages/Explore.jsx
+++ Frontend/src/features/posts/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import '../style/explore.css'
+import '../style/Explore.css'
+
 
 const api = axios.create({
     baseURL: "http://localhost:3000",
--- Frontend/src/features/posts/services/follow.api.js
+++ Frontend/src/features/posts/services/follow.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://insta-xz.onrender.com",
     withCredentials: true
 })
 
