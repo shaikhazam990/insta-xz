3f57c582e791c0ea5be4e3d521c63e2670084225
shaikhazam990 <shaikhazam0990@gmail.com>
2026-03-01 19:54:41 +0530

    url correct

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,32 +1,28 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://insta-xz.onrender.com",
-    withCredentials:true,
+  baseURL: "https://insta-xz.onrender.com",
+  withCredentials: true,
 })
 
-export async function login(username,password){
-
-    const response = await api.post('/login',{
-        username, password
-    })
-
-    return response.data
+export async function login(username, password) {
+  const response = await api.post('/api/auth/login', {
+    username,
+    password,
+  })
+  return response.data
 }
 
 export async function register(username, email, password) {
-
-    const response = await api.post('/register',{
-        username,password,email
-    })
-
-    return response.data
-    
+  const response = await api.post('/api/auth/register', {
+    username,
+    email,
+    password,
+  })
+  return response.data
 }
 
-export async function getMe(){
-    const response = await api.get('/get-me')
-
-    return response.data
-    
+export async function getMe() {
+  const response = await api.get('/api/auth/get-me')
+  return response.data
 }
